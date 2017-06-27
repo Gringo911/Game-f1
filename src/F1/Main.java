9178c6765e9abe4d13eb2ee0318d4d780eb616d2
vasy911 <irbisaltai@yandex.ua>
2017-06-27 21:23:48 +0200

    telo

---
+++ src/F1/Main.java
@@ -0,0 +1,15 @@
+package F1;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		JFrame f = new JFrame("Java F1");
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setSize(1100,600);
+		f.setVisible(true);
+
+	}
+
+}
